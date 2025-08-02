26dbf8205070f020a05d27fc8041b63411ba6663
feykke <re.office.25@gmail.com>
2025-08-02 17:27:42 -0300

    Starting quarkus project

---
+++ src/test/java/org/acme/MainAppIT.java
@@ -0,0 +1,8 @@
+package org.acme;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class MainAppIT extends MainAppTest {
+    // Execute the same tests but in packaged mode.
+}
