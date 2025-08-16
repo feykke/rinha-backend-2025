765d2bf3c5a0bdf5004223140445ee04169cc246
feykke <re.office.25@gmail.com>
2025-08-16 00:23:01 -0300

    Creating DateUtils with parse method

---
+++ src/main/java/org/acme/utils/DateUtils.java
@@ -0,0 +1,16 @@
+package org.acme.utils;
+
+import java.time.Instant;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+
+public final class DateUtils {
+
+    private final static DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").withZone(ZoneOffset.UTC);
+
+
+    public static Long parseIsoUtcToEpochMilli(String isoUtcString) {
+        if (isoUtcString == null || isoUtcString.isBlank()) return null;
+        return Instant.from(FORMATTER.parse(isoUtcString)).toEpochMilli();
+    }
+}
