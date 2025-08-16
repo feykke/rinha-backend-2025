330f2df1997e4d926a52949fa857bc5add5a0347
feykke <re.office.25@gmail.com>
2025-08-16 00:26:53 -0300

    Updating configuration and finishing payment methods

--- src/main/java/org/acme/resource/PaymentsResource.java
+++ src/main/java/org/acme/resource/PaymentsResource.java
@@ -5,17 +5,18 @@ import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.Response;
 import org.acme.dto.PaymentRequestDTO;
-import org.acme.dto.PaymentsSummaryResponseDTO;
+import org.acme.entity.PaymentsSummaryResponse;
 import org.acme.service.PaymentsService;
 
 
 @Path("/")
 public class PaymentsResource {
 
-
     private final PaymentsService paymentsService;
+
     @Inject
     public PaymentsResource(PaymentsService paymentsService) {
         this.paymentsService = paymentsService;
@@ -23,8 +24,12 @@ public class PaymentsResource {
 
     @Path("payments-summary")
     @GET
-    public Response getPaymentsSummary() {
-        PaymentsSummaryResponseDTO summary = new PaymentsSummaryResponseDTO(12, 14, 150.0, 31.00);
+    public Response getPaymentsSummary(
+            @QueryParam("from") final String from,
+            @QueryParam("to") final String to
+    ) {
+
+        PaymentsSummaryResponse summary = paymentsService.getPaymentsSummary(from, to);
         return Response.status(HttpResponseStatus.OK.code()).entity(summary).build();
     }
 
@@ -32,6 +37,6 @@ public class PaymentsResource {
     @POST
     public Response postPayment(PaymentRequestDTO payment) {
         paymentsService.enqueuePayment(payment);
-        return Response.status(HttpResponseStatus.CREATED.code()).build();
+        return Response.status(HttpResponseStatus.ACCEPTED.code()).build();
     }
 }
--- src/main/java/org/acme/service/PaymentsService.java
+++ src/main/java/org/acme/service/PaymentsService.java
@@ -1,23 +1,122 @@
 package org.acme.service;
 
+import io.quarkus.scheduler.Scheduled;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import org.acme.dto.HealthCheckResponseDTO;
 import org.acme.dto.PaymentRequestDTO;
+import org.acme.entity.Payment;
+import org.acme.entity.PaymentQueueItens;
+import org.acme.entity.PaymentsSummaryResponse;
+import org.acme.entity.ProcessorSummary;
+import org.acme.repository.processors.PaymentProcessorDefault;
+import org.acme.repository.processors.PaymentProcessorFallback;
 import org.acme.repository.redis.RedisRepository;
+import org.acme.repository.redis.dbo.PaymentDBO;
+import org.acme.utils.DateUtils;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.jboss.logging.Logger;
+import org.jboss.resteasy.reactive.RestResponse;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
 @ApplicationScoped
 public class PaymentsService {
 
+    private final Logger LOG = Logger.getLogger(PaymentsService.class);
+
+    private final String DEFAULT = "default";
+    private final String FALLBACK = "fallback";
+
+    private final AtomicReference<String> activeProcessor = new AtomicReference<>(DEFAULT);
     private final RedisRepository redisRepository;
+    private final PaymentProcessorDefault paymentProcessorDefault;
+    private final PaymentProcessorFallback paymentProcessorFallback;
 
     @Inject
     public PaymentsService(
-            RedisRepository redisRepository
+            RedisRepository redisRepository,
+            @RestClient PaymentProcessorDefault paymentProcessorDefault,
+            @RestClient PaymentProcessorFallback paymentProcessorFallback
     ) {
         this.redisRepository = redisRepository;
+        this.paymentProcessorDefault = paymentProcessorDefault;
+        this.paymentProcessorFallback = paymentProcessorFallback;
+    }
+
+
+    public PaymentsSummaryResponse getPaymentsSummary(String from, String to) {
+        Long fromMilli = DateUtils.parseIsoUtcToEpochMilli(from);
+        Long toMilli = DateUtils.parseIsoUtcToEpochMilli(to);
+
+        List<PaymentDBO> payments = redisRepository.getPayments(fromMilli, toMilli);
+
+        ProcessorSummary summarizedDefault = summarizeProcessors(payments, DEFAULT);
+        ProcessorSummary summarizedFallback = summarizeProcessors(payments, FALLBACK);
+
+        return new PaymentsSummaryResponse(summarizedDefault, summarizedFallback);
     }
 
     public void enqueuePayment(final PaymentRequestDTO payment) {
         redisRepository.enqueue(payment);
+    }
+
+    public PaymentQueueItens dequeuePayment() {
+        return redisRepository.dequeue();
+    }
+
+    public void processPayment(Payment payment) {
+        if(activeProcessor.get().equals(DEFAULT)) {
+            RestResponse<Void> response =  paymentProcessorDefault.processPayment(payment);
+            handlePaymentResponse(response, payment, DEFAULT);
+            return;
+        }
+
+        if(activeProcessor.get().equals(FALLBACK)) {
+            RestResponse<Void> response = paymentProcessorFallback.processPayment(payment);
+            handlePaymentResponse(response, payment, FALLBACK);
+            return;
+        }
+
+        redisRepository.enqueue(new PaymentRequestDTO(payment.getCorrelationId(), payment.getAmount()));
+        LOG.debugf("All processors are down, enqueuing payment %s", payment.getCorrelationId());
+    }
+
+    @Scheduled(every = "5s")
+    public void checkProcessorsHealth() {
+        RestResponse<HealthCheckResponseDTO> healthDefault = paymentProcessorDefault.healthCheck();
+        RestResponse<HealthCheckResponseDTO> healthFallback = paymentProcessorFallback.healthCheck();
+        if (!healthDefault.getEntity().failing()) {
+            activeProcessor.set("default");
+            return;
+        }
+        if (!healthFallback.getEntity().failing()) {
+            activeProcessor.set("fallback");
+            return;
+        }
+
+        activeProcessor.set("none");
+    }
+
+    private void handlePaymentResponse(RestResponse<Void> response, Payment payment, String processor) {
+        if (response.getStatus() == 200) {
+            redisRepository.savePayment(payment, processor);
+        } else {
+            redisRepository.enqueue(new PaymentRequestDTO(payment.getCorrelationId(), payment.getAmount()));
+            LOG.errorf("Error creating payment in %s processor", processor);
+        }
+    }
+
+    private ProcessorSummary summarizeProcessors (List<PaymentDBO> payments, String processor) {
+        List<PaymentDBO> paymentsByProcessor = payments.stream()
+                .filter(payment -> processor.equals(payment.getProcessor())).toList();
+
+        BigDecimal totalAmount = paymentsByProcessor.stream()
+                .map(PaymentDBO::getAmount)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+        return new ProcessorSummary(paymentsByProcessor.size(), totalAmount);
     }
 }
