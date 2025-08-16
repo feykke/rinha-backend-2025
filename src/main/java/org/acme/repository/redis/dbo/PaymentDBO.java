f7cff660ce25c885807e282ba86ba65c50854f6c
feykke <re.office.25@gmail.com>
2025-08-16 00:23:32 -0300

    Creating PaymentDBO representation

---
+++ src/main/java/org/acme/repository/redis/dbo/PaymentDBO.java
@@ -0,0 +1,31 @@
+package org.acme.repository.redis.dbo;
+
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@RegisterForReflection
+public class PaymentDBO {
+    private final UUID correlationId;
+    private final BigDecimal amount;
+    private final String processor;
+
+    public PaymentDBO(UUID correlationId, BigDecimal amount, String processor) {
+        this.correlationId = correlationId;
+        this.amount = amount;
+        this.processor = processor;
+    }
+
+    public UUID getCorrelationId() {
+        return correlationId;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public String getProcessor() {
+        return processor;
+    }
+}
